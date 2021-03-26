0f57c2ef9ccda229d4493df0df78896ed0f65bf5
koalter <heavens_drive090@hotmail.com>
2021-03-25 23:40:32 -0300

    mvp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NativeAudio } from '@ionic-native/native-audio/ngx';
 import { TimerService } from './services/timer.service';
 
 @Component({
@@ -8,19 +9,24 @@ import { TimerService } from './services/timer.service';
 })
 export class AppComponent {
 
-  audio: HTMLAudioElement = new Audio('../assets/bensound-creativeminds.mp3');
+  // audio: HTMLAudioElement = new Audio('../assets/bensound-creativeminds.mp3');
 
-  constructor(private timerService: TimerService) {
-    const timer = setInterval(() => {
-      console.log(new Date(Date.now()).toTimeString())
-      for (let counter of this.timerService.counterList) {
-        if (Math.floor(Date.parse(new Date(counter.endTime).toISOString())/1000) - Math.floor(Date.now()/1000) <= 0) this.audio.play().then(() => {
-          if (Math.floor(Date.parse(new Date(counter.endTime).toISOString())/1000 + 10) - Math.floor(Date.now()/1000) <= 0) {
-            this.audio.pause();
-            this.timerService.counterList.splice(this.timerService.counterList.findIndex(c => c === counter), 1);
-          }
-        });
-      }
-    }, 1000);
+  constructor(private audio: NativeAudio,
+              private timerService: TimerService) {
+    this.audio.preloadComplex('alarm', '../assets/bensound-creativeminds.mp3', 1, 1, 0).then(() => {
+
+      const timer = setInterval(() => {
+        console.log(new Date(Date.now()).toTimeString())
+        for (let counter of this.timerService.counterList) {
+          if (Math.floor(Date.parse(new Date(counter.endTime).toISOString())/1000) - Math.floor(Date.now()/1000) <= 0) this.audio.play('alarm').then(() => {
+            if (Math.floor(Date.parse(new Date(counter.endTime).toISOString())/1000 + 10) - Math.floor(Date.now()/1000) <= 0) {
+              this.audio.stop('alarm').then(() => {
+                this.timerService.counterList.splice(this.timerService.counterList.findIndex(c => c === counter), 1);
+              });
+            }
+          });
+        }
+      }, 1000);
+    });
   }
 }
